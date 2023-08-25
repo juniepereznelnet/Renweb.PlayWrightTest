bd739798b9d5b2574ff58cda719a4de5560d1810
Junie Perez <110467@nelnet.net>
2023-08-25 15:18:32 +0800

    Created Login Test

---
+++ tests/login.test.ts
@@ -0,0 +1,19 @@
+import test, { Browser, BrowserContext, expect, chromium, Page } from "@playwright/test";
+
+
+test("Login RenWeb", async () => {
+
+    const browser = await chromium.launch({
+        headless: false
+    });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto("https://sp.renweb.com/session/login/");
+    await page.locator('#mat-input-0').type("110467@nelnet.net");
+    await page.locator('#mat-input-1').type("Q623pass.");
+
+    await page.click('span.mat-button-wrapper');
+
+    await expect(page).toHaveURL("https://sp.renweb.com/impersonate");
+})
